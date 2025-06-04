dad96bee3b5c7553fad19b8be9185d75ba337e16
CamilaGonzalezPalese <62571300+CamilaGonzalezPalese@users.noreply.github.com>
2025-06-04 18:47:44 -0300

    Subo el backend separado del frontend

---
+++ frontend/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './pages/HomePages';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <HomePage />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
